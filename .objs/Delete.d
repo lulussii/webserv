f0849c521ffcbe07064a8ca94da05f87cad6d8fa
Mathilde Laussel <mlaussel@z3r12p1.42lyon.fr>
2026-02-09 12:34:02 +0100

    must link listen and cgi part between mate

---
+++ .objs/Delete.d
@@ -0,0 +1,15 @@
+.objs/Delete.o: src/Delete.cpp include/Delete.hpp include/Request.hpp \
+  include/Multipart.hpp include/Response.hpp include/Config.hpp \
+  include/Error.hpp
+
+include/Delete.hpp:
+
+include/Request.hpp:
+
+include/Multipart.hpp:
+
+include/Response.hpp:
+
+include/Config.hpp:
+
+include/Error.hpp:
---
+++ .objs/Init.d
@@ -0,0 +1,12 @@
+.objs/Init.o: src/Init.cpp include/Config.hpp include/Request.hpp \
+  include/Multipart.hpp include/Response.hpp include/Cgi.hpp
+
+include/Config.hpp:
+
+include/Request.hpp:
+
+include/Multipart.hpp:
+
+include/Response.hpp:
+
+include/Cgi.hpp:
